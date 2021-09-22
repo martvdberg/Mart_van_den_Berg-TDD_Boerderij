d3af426e86cb6f6a9b1d1d481bd1785e67a0e1a1
Mart van den Berg <martvdberg@gmail.com>
2021-09-22 21:28:41 +0200

    getYieldForCrop works

--- farm.js
+++ farm.js
@@ -1,7 +1,17 @@
 const getYieldForPlant = (plant) => {
-  return plant.yield;
+  //   check if its a simple or a more complex object structur based on test provided by Winc
+  if (plant.hasOwnProperty("yield")) {
+    const basicYield = plant.yield;
+    return basicYield;
+  } else if (plant.crop.hasOwnProperty("yield")) {
+    const basicYield = plant.crop.yield;
+    return basicYield;
+  }
 };
+
+const getYieldForCrop = (crop) => getYieldForPlant(crop) * crop.numCrops;
 
 module.exports = {
   getYieldForPlant,
+  getYieldForCrop,
 };
